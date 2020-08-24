c9d20f996cafbe115341c164709d5372a7a2a954
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-08-24 18:35:21 +0300

    defined project structure

---
+++ config/init.php
@@ -0,0 +1,11 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/shop/core');
+define("LIBS", ROOT . '/vendor/shop/core/libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'default');
