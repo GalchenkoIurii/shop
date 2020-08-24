356698235ac0bda75f9cc7d6fb0b823007354fa3
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-08-24 18:55:51 +0300

    added init.php

--- config/init.php
+++ config/init.php
@@ -9,3 +9,11 @@ define("LIBS", ROOT . '/vendor/shop/core/libs');
 define("CACHE", ROOT . '/tmp/cache');
 define("CONFIG", ROOT . '/config');
 define("LAYOUT", 'default');
+
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+$app_path = preg_replace("#[^/]+$#", '', $app_path);
+$app_path = str_replace('/public/', '', $app_path);
+define("PATH", $app_path);
+define("ADMIN", PATH . '/admin');
+
+require_once ROOT . '/vendor/autoload.php';
