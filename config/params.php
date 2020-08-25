4fd16023bc4ce76dc7b7768a28c46b31216a1fa1
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2020-08-25 21:54:53 +0300

    changed App.php, Registry.php and added params.php

---
+++ config/params.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Iurii
+ * Date: 25.08.2020
+ * Time: 21:36
+ */
+
+return [
+    'admin_email' => 'admin@gmail.com',
+    'site_name' => 'EShop',
+    'pagination' => 3,
+    'smtp_login' => '',
+    'smtp_password' => '',
+];
--- vendor/shop/core/App.php
+++ vendor/shop/core/App.php
@@ -17,5 +17,17 @@ class App
     {
         $query = trim($_SERVER['QUERY_STRING'], '/');
         session_start();
+        self::$app = Registry::instance();
+        $this->getParams();
+    }
+
+    protected function getParams()
+    {
+        $params = require_once CONFIG . '/params.php';
+        if (!empty($params)) {
+            foreach ($params as $k => $v) {
+                self::$app->setProperty($k, $v);
+            }
+        }
     }
 }
